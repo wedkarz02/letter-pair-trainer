d4514685c95e87475ada467259b4dca1a3a60de4
Paweł Rybak <pawel.rybak13@gmail.com>
2022-09-04 20:53:40 +0200

    Invalid input file protection added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import os
+import sys
 import time
 import random
 
@@ -8,3 +9,14 @@ try:
 except:
     print("\033[91m[ERROR]\033[0m: openpyxl not installed.")
     os._exit()
+
+if os.path.exists(sys.argv[1]):
+    path = sys.argv[1]
+    if not path.endswith(".xlsx"):
+        print("\033[91m[ERROR]\033[0m: Invalid file privided. See README.md for more information.")
+        os._exit()
+else:
+    print(f"\033[91m[ERROR]\033[0m: File '{sys.argv[1]}' does not exist.")
+    os._exit()
+
+workbook = openpyxl.load_workbook(path)
