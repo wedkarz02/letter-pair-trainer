edbe7075e2ac714c5a3bc7178ef006d798c4a4ca
Paweł Rybak <pawel.rybak13@gmail.com>
2022-09-04 21:08:15 +0200

    Input protection updated

--- main.py
+++ main.py
@@ -7,16 +7,26 @@ import random
 try:
     import openpyxl
 except:
-    print("\033[91m[ERROR]\033[0m: openpyxl not installed.")
-    os._exit()
+    print("\033[91m[ERROR]\033[0m: openpyxl not installed.\n")
+    os._exit(1)
 
-if os.path.exists(sys.argv[1]):
-    path = sys.argv[1]
-    if not path.endswith(".xlsx"):
-        print("\033[91m[ERROR]\033[0m: Invalid file privided. See README.md for more information.")
-        os._exit()
-else:
-    print(f"\033[91m[ERROR]\033[0m: File '{sys.argv[1]}' does not exist.")
-    os._exit()
+try:
+    if os.path.exists(sys.argv[1]):
+        path = sys.argv[1]
+        if not path.endswith(".xlsx"):
+            print("\033[91m[ERROR]\033[0m: Invalid file privided. See README.md for more information.\n")
+            os._exit(1)
+    else:
+        print(f"\033[91m[ERROR]\033[0m: File '{sys.argv[1]}' does not exist.\n")
+        os._exit(1)
+except:
+    print(f"\033[91m[ERROR]\033[0m: Invalid arguments provided. See README.md for more information.\n")
+    os._exit(1)
 
 workbook = openpyxl.load_workbook(path)
+
+try:
+    sheet = workbook["Letter"]
+except:
+    print("\033[91m[ERROR]\033[0m: No sheet named 'Letter Pairs' found. See README.md for more information.\n")
+    os._exit(1)
